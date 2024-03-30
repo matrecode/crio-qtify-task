4a80190933e901085a3f7950b09f52c887932193
matrecode <akshaymatrecode@gmail.com>
2024-03-30 14:34:18 +0530

    New albums added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
-import Card from "./components/Cards/Card";
-import { fetchTopAlbums } from "./api/api";
+import { fetchTopAlbums, fetchNewAlbums } from "./api/api";
+import { useState } from "react";
 import Section from "./components/Section/Section";
 import styles from "./App.module.css";
 
 function App() {
   const [topAlbumData, setTopAlbumData] = useState([]);
+  const [newAlbumData, setNewAlbumData] = useState([]);
 
   const generateTopAlbumData = async () => {
     const data = await fetchTopAlbums();
-    console.log("data", data);
+    // console.log("data", data);
     setTopAlbumData(data);
+  };
+
+  const generateNewAlbumData = async () => {
+    const data = await fetchNewAlbums();
+    setNewAlbumData(data);
   };
 
   useEffect(() => {
     generateTopAlbumData();
+    generateNewAlbumData();
   }, []);
+
   return (
     <>
       <Navbar />
       <HeroSection />
-      <Card />
       <div className={styles.sectionWrapper}>
         <Section title="Top Albums" data={topAlbumData} type="album" />
+        <Section title="New Albums" data={newAlbumData} type="album" />
       </div>
     </>
   );
 }
-
 export default App;
